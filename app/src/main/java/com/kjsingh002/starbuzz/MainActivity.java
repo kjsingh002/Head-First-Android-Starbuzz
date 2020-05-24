21c3065869c4cee2576cbd2eeec485c1af907f2f
kjsingh002 <58228293+kjsingh002@users.noreply.github.com>
2020-05-24 07:28:41 +0530

    drinkCategoryactivity added

--- app/src/main/java/com/kjsingh002/starbuzz/MainActivity.java
+++ app/src/main/java/com/kjsingh002/starbuzz/MainActivity.java
@@ -2,13 +2,29 @@ package com.kjsingh002.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView itemsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        itemsList = findViewById(R.id.list_of_items);
+        itemsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    final Intent intent = new Intent(MainActivity.this,DrinksCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
