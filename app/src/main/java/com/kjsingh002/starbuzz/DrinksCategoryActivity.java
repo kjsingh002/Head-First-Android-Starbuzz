90b1ee3716d78c2c7f079f1ea6b98796db882cc9
kjsingh002 <58228293+kjsingh002@users.noreply.github.com>
2020-05-24 07:38:38 +0530

    drinkactivity added

--- app/src/main/java/com/kjsingh002/starbuzz/DrinksCategoryActivity.java
+++ app/src/main/java/com/kjsingh002/starbuzz/DrinksCategoryActivity.java
@@ -3,12 +3,15 @@ package com.kjsingh002.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class DrinksCategoryActivity extends AppCompatActivity {
+    private ListView drinksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks_category);
+        drinksList = findViewById(R.id.list_of_drinks);
     }
 }
